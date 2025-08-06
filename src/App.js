bdfb698378a73e18bbf7fc8edc187f73efd030dd
Daniel Oulton <daniel.oulton@gmail.com>
2025-08-06 18:05:41 -0400

    added layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import StaffPage from './pages/StaffPage.jsx';
+import Layout from "./components/Layout";
+
 
 function App() {
   return (
     <Router>
+     <Layout>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/staff" element={<StaffPage />} />
       </Routes>
+     </Layout>
     </Router>
   );
 }
