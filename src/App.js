04bbcb8747ece4e5a890e4b0124c48029626c5f0
Daniel Oulton <daniel.oulton@gmail.com>
2025-08-06 12:50:43 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Event Staffing Portal</h1>
+      <p>This is the frontend. You can now build out features here.</p>
+    </div>
+  );
+}
+
+export default App;
